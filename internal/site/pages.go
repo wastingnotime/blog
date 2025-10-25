9d648912a600d5dfb12d52fb41f5fc48928688b6
hriccio <hriccio@wastingnotime.org>
2025-10-25 18:09:28 -0300

    Add breadcrumbs to saga arc and episode pages

---
+++ internal/site/pages.go
@@ -0,0 +1,50 @@
+package site
+
+import (
+	"html/template"
+	"time"
+)
+
+type ArcLink struct {
+	Title string
+	URL   string
+}
+
+type EpisodeLink struct {
+	Title  string
+	URL    string
+	Number int
+}
+
+type ArcPageData struct {
+	Section       string
+	NowYear       int
+	SagaTitle     string
+	SagaURL       string
+	ArcTitle      string
+	ArcSummary    string
+	Episodes      []*EpisodeRef
+	FirstEpisode  *EpisodeRef
+	LatestEpisode *EpisodeRef
+	PrevArc       *ArcLink
+	NextArc       *ArcLink
+	LastRelease   *time.Time
+}
+
+type EpisodePageData struct {
+	Section            string
+	NowYear            int
+	SagaTitle          string
+	SagaURL            string
+	ArcTitle           string
+	ArcURL             string
+	EpisodeTitle       string
+	EpisodeNumber      int
+	EpisodeSummary     string
+	EpisodeAuthor      string
+	EpisodeDate        time.Time
+	EpisodeReadingTime string
+	EpisodeBody        template.HTML
+	PrevEpisode        *EpisodeLink
+	NextEpisode        *EpisodeLink
+}
